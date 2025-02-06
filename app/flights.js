c5f91cef4bd31fe72b5dfd8c277bc81d3b10c46d
osama-25 <osamasami208@gmail.com>
2025-02-06 15:47:14 +0400

    fix

--- app/flights.js
+++ app/flights.js
@@ -147,7 +147,7 @@ export default function Flights() {
                         /></>)}
                     </div>
                 </div>
-                <div className="absolute bottom-[-24] left-0 w-full flex justify-center items-center">
+                <div className="absolute -bottom-6 left-0 w-full flex justify-center items-center">
                     <div className="flex justify-center items-center gap-x-2 p-2 px-3 bg-blue-600 hover:bg-blue-700 cursor-pointer rounded-full w-[100] h-10">
                         <svg className="fill-white" width="24" height="24" viewBox="0 0 24 24" focusable="false"><path d="M20.49 19l-5.73-5.73C15.53 12.2 16 10.91 16 9.5A6.5 6.5 0 1 0 9.5 16c1.41 0 2.7-.47 3.77-1.24L19 20.49 20.49 19zM5 9.5C5 7.01 7.01 5 9.5 5S14 7.01 14 9.5 11.99 14 9.5 14 5 11.99 5 9.5z"></path></svg>
                         <span className="text-white text-sm font-semibold">Explore</span>
